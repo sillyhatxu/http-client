81c4f81cc77c0503198cd06fd0df458ebbcae650
sillyhat <xushikuan1@163.com>
2019-10-05 23:41:59 +0800

    add logs

--- client_test.go
+++ client_test.go
@@ -1,6 +1,8 @@
 package client
 
 import (
+	"bytes"
+	"encoding/json"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
@@ -15,5 +17,21 @@ func TestDoGet(t *testing.T) {
 	}
 	var obj Obj
 	err := response.AnalysisBody(&obj)
+	assert.Nil(t, err)
+}
+
+func TestDoPost(t *testing.T) {
+	httpClient := NewHttpClient(host, ShowResponseLog(true))
+	var input interface{}
+	inputJSON, err := json.Marshal(input)
+	if err != nil {
+		panic(err)
+	}
+	response := httpClient.DoPost("", bytes.NewBuffer(inputJSON))
+	assert.Nil(t, response.Error)
+	type Obj struct {
+	}
+	var obj Obj
+	err = response.AnalysisBody(&obj)
 	assert.Nil(t, err)
 }
